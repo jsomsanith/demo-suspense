3bcb27042ad9e451407955323348c9a9a6f3e545
Jimmy Somsanith <jsomsanith@talend.com>
2019-06-07 23:44:02 +0200

    Suspense for code splitting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import HeaderBar from './HeaderBar';
-
+import Loading from './Loading';
 import NoList from './List/NoList';
-import PokemonList from './List/PokemonList';
-import StarWarsList from './List/StarWarsList';
 
 import ListCategories from './List/categories';
 import './App.css';
+
+const PokemonList = React.lazy(() =>
+	import(/* webpackChunkName: "pokemon-list" */ './List/PokemonList'),
+);
+const StarWarsList = React.lazy(() =>
+	import(/* webpackChunkName: "starwars-list" */ './List/StarWarsList'),
+);
 
 function App() {
 	const [menu, setMenu] = useState();
@@ -16,9 +21,11 @@ function App() {
 			<HeaderBar onMenuChange={setMenu} />
 
 			<div className="body">
-				{!menu && <NoList />}
-				{menu === ListCategories.POKEMONS && <PokemonList />}
-				{menu === ListCategories.STAR_WARS && <StarWarsList />}
+				<Suspense fallback={<Loading />}>
+					{!menu && <NoList />}
+					{menu === ListCategories.POKEMONS && <PokemonList />}
+					{menu === ListCategories.STAR_WARS && <StarWarsList />}
+				</Suspense>
 			</div>
 		</div>
 	);
